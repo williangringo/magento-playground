2609f5c3b4824b69ad647b1a7402495bad48bc19
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2014-01-09 10:06:34 +0100

    usable frontend

---
+++ app/code/local/Inchoo/Weblog/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Inchoo_Weblog_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
--- app/code/local/Inchoo/Weblog/controllers/PostsController.php
+++ app/code/local/Inchoo/Weblog/controllers/PostsController.php
@@ -7,12 +7,14 @@ class Inchoo_Weblog_PostsController extends Mage_Core_Controller_Front_Action
      */
     public function indexAction()
     {
-        $posts = Mage::getModel('weblog/blogpost')->getcollection();
+        /**
+         * Fetch data from model, store it to register for layout
+         */
+        $blog = Mage::getModel('weblog/blogpost')->getCollection();
+        Mage::register('blog_posts', $blog);
 
-        foreach($posts as $post) {
-            echo '<h3><a href="http://playground.loc/weblog/posts/view/id/', $post->getBlogpost_id(), '">', $post->getTitle(), '</a></h3>';
-            echo '<h6>', $post->getDate(), '</h6>';
-        }
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
     /**
@@ -21,29 +23,26 @@ class Inchoo_Weblog_PostsController extends Mage_Core_Controller_Front_Action
     public function viewAction()
     {
         /**
-         * FIXME: How to check for empty result?
+         * Fetch Id from request, blogpost from db
          */
-         $id = $this->getRequest()->getParam('id');
-         $blogpost = Mage::getModel('weblog/blogpost');
-         $blogpost->load($id);
-
-        echo '<h2>', $blogpost->getTitle(), '</h2>';
-        echo '<h6>', $blogpost->getDate(), '</h6>';
-        echo '<span><a href="http://playground.loc/weblog/posts/delete/id/', $blogpost->getBlogpost_id(), '">delete</a></span>';
-        echo ' - ';
-        echo '<span><a href="http://playground.loc/weblog/posts/edit/id/', $blogpost->getBlogpost_id(), '">edit</a></span>';
-        echo '<p>', nl2br($blogpost->getPost()), '</p>';
-    }
-
-    public function deleteAction()
-    {
-        $id = $this->getRequest()->getParam('id');
-        $post = Mage::getModel('weblog/blogpost');
-        $post->load($id);
-        $post->delete();
+        $id = $this->getRequest()->getPAram('id');
+        $blogpost = Mage::getModel('weblog/blogpost');
+        $blogpost->load($id);
 
         /**
-         * FIXME: Redirect ??
+         * 404 if post can't be loaded
          */
+        if ($blogpost->getId() == null) {
+            $this->_forward('noRoute');
+            return;
+        }
+
+        /**
+         * Store in register for layout
+         */
+        Mage::register('blog_post', $blogpost);
+
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
